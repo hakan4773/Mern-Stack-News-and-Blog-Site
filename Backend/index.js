24f2e0abf198299e0ef63dd2a32b6da32495d4ba
hakan4773 <hb480312@gmail.com>
2024-12-10 18:24:08 +0300

    project updated 15

--- Backend/index.js
+++ Backend/index.js
@@ -47,7 +47,7 @@ app.use(cookieParser());
 app.use(fileUpload());
 app.use(express.static("public"))
 app.use(methodOverride("_method",{methods:["GET","POST"]}))
-app.use(express.static(path.join(__dirname, '../frontend/build')));
+
 
 
 app.use("/",pageRoutes)
@@ -59,7 +59,7 @@ app.use("/contact",contactRoutes)
 // app.use("/comment",commentRoutes)
 
 app.use("/Admin",adminRoutes)
-
+app.use(express.static(path.join(__dirname, '../frontend/build')));
 // Tüm yönlendirmeleri React uygulamasına yönlendirin
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'))
