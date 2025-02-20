39e0fcfd00a1f26cfd1878974cf0e526860300e9
Coders Club <hi@codersclub.com>
2025-02-20 18:15:32 +0300

    backend cors hatası 8

--- Backend/index.js
+++ Backend/index.js
@@ -46,6 +46,8 @@ app.use(cors({
   origin: ['https://news-mern-stack-news-and-blog-site.netlify.app', 'http://localhost:3000'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ["Content-Type", "Authorization"]
+
 }));
 
 
